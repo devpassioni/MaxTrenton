ac1134cb60a26b6dd763221144d91ac6acc90924
Jonathan Passioni <jhow-rock@live.com>
2026-02-15 16:53:22 -0300

    subindo motorCycleService apos atualizacao

--- backend/src/service/motorcycleService.ts
+++ backend/src/service/motorcycleService.ts
@@ -5,8 +5,6 @@ export class MotorcycleService extends JsonRepository<Motorcycle>{
     constructor(){
         super("resources/data/motorcycle.json")
     }
-
-
 public async addMoto(moto: Motorcycle): Promise<void>{
     const motorcycle = await this.load();
     motorcycle.push(moto);
@@ -16,10 +14,63 @@ public async addMoto(moto: Motorcycle): Promise<void>{
 public async removeMoto(id: number): Promise<boolean>{
     const motorcycle = await this.load();
     const index = motorcycle.findIndex(moto => moto.id === id)
-    motorcycle.slice(index,1);
+    motorcycle.splice(index,1);
     await this.save(motorcycle);
     return true;
 
 }
+
+public async getMotorCycleById(id: number): Promise<Motorcycle>{
+    const motorcycle = await this.load();
+    const finder = motorcycle.find(moto => moto.id === id)
+    
+    if(!finder) throw new Error(`Motorcycle id ${id} not founded`)
+    
+    return finder
+}
+
+public async getAllMotorcycle(): Promise<Motorcycle[]>{
+    return await this.load()
+    
+}
+public async getAllAvaialableMotorcycle(): Promise<Motorcycle[]>{
+    const motorcycle = await this.load();
+    const findex = motorcycle.filter(motos => motos.status === "Available")
+    return findex
+}
+
+public async getMotorcycleByBrand(brand: string): Promise<Motorcycle []>{
+    const motorcycle = await this.load();
+    const findex = motorcycle.filter(motos => motos.brand === brand)
+    return findex
+}
+
+public async getMotorcycleByType(type: string): Promise<Motorcycle []>{
+    const motorcycle = await this.load()
+    const findex = motorcycle.filter(motos => motos.bodyType === type)
+    return findex
+}
+
+public async getMotorcycleByPriceRange(min: number, max: number): Promise<Motorcycle[]>{
+    const motorcycle = await this.load()
+    const findex = motorcycle.filter(motos => motos.price >= min && motos.price <= max)
+    return findex
+}
+
+public async updateMotorcyclePrice(id: number, newPrice: number): Promise<void>{
+    const motorcycle = await this.load()
+    let findex = motorcycle.find(moto => moto.id === id)
+    if(!findex){
+        throw new Error(`Error, motorcycle ${id} not founded!`)
+    }
+    findex.price = newPrice;
+    //verificar possibilidade nesse bug - fiz muito rapido e ja estou cansado!
+    await this.save(motorcycle)
+}
+
+public async updateMotorcycleStatus(id: number, status: "Available" | "Sold" | "Reserved"): Promise<void>{
+    
+}
+
 
 }
