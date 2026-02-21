67dc33738770da33de8fd26ed85048875699c08c
Jonathan Passioni <jhow-rock@live.com>
2026-02-21 11:18:57 -0300

    startando dealService API - Jonathan

--- api/src/deal/deal.service.ts
+++ api/src/deal/deal.service.ts
@@ -1,3 +1,63 @@
 import {Injectable} from "@nestjs/common"
-import {DealService} from "../../../backend/src/service/dealService"
+import {DealService as BackendDealService} from "../../../backend/src/service/dealService"
 import {SellerService} from  "../../../backend/src/service/sellerService"
+import {CarService} from "../../../backend/src/service/carService"
+import {MotorcycleService} from "../../../backend/src/service/motorcycleService"
+import {CustomerService} from "../../../backend/src/service/customerService"
+
+
+@Injectable()
+export class DealServiceAPI{
+    private dealService: BackendDealService;
+
+    constructor(){
+        const carService = new CarService()
+        const motoService = new MotorcycleService()
+        const customerService = new CustomerService()
+        const sellerService = new SellerService() 
+    
+        this.dealService = new BackendDealService(
+            carService,
+            motoService,
+            sellerService,
+            customerService,
+    
+        )
+    }
+
+    async findAll(){
+        return this.dealService.getallDeals();
+    }
+
+
+    async addDeal(){
+        return this.dealService.addDeal // <- Precisara de um DTO
+    }
+
+
+    async removeDeal(id: number){
+        return this.dealService.removeDeal(id);
+    }
+
+    async getDealById(id:number){
+        return this.dealService.getDealById(id);
+    }
+
+
+    async getDealBySeller(sellerId:number){
+        return this.dealService.getDealById(sellerId);
+    }
+
+    async getPendingDeals(){
+        return this.dealService.getPendingDeals();
+    }
+
+    async getApprovedDeals(){
+        return this.dealService.getApprovedDeals();
+    }
+
+    async getTotalRevenue(){
+        return this.dealService.getTotalRevenue();
+    }
+
+}
